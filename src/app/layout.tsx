9b833dbcf7110cef1928c5112592ab23b798f394
kyrylolvov <kyrylo@arccompute.io>
2025-02-27 12:17:19 -0500

    CHANGED: - Added "DM Sans" font - Added tailwind theme to match colors in the design
    Signed-off-by: kyrylolvov <kyrylo@arccompute.io>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import "./globals.css";
 
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const sansFont = DM_Sans({
+  variable: "--font-dm-sans",
   subsets: ["latin"],
 });
 
@@ -25,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${sansFont.variable} antialiased`}>{children}</body>
     </html>
   );
 }
