29978b345e278d09148348edf073b6002a67290c
kyrylolvov <kyrylo@arccompute.io>
2025-02-27 12:07:48 -0500

    CHANGED: - Installed eslint/prettier plugins - Updated eslint and prettier configs - Removed default boilerplate
    Signed-off-by: kyrylolvov <kyrylo@arccompute.io>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,13 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import prettier from "eslint-plugin-prettier";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +18,26 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "unused-imports": unusedImports,
+      "simple-import-sort": simpleImportSort,
+      "react-hooks": reactHooks,
+      "react": react,
+      "prettier": prettier,
+    },
+    rules: {
+      "unused-imports/no-unused-imports": "error",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "react/prop-types": "off",
+      "react/no-unknown-property": "off",
+      "react/react-in-jsx-scope": "off",
+      "prettier/prettier": "error",
+      "@typescript-eslint/no-empty-object-type": "off"
+    },
+    ignores: ["*.mjs"],
+  },
 ];
 
 export default eslintConfig;
---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+const prettierConfig = {
+    semi: true,
+    trailingComma: "all",
+    singleQuote: false,
+    printWidth: 150,
+    tabWidth: 2,
+    plugins: ["prettier-plugin-tailwindcss"],
+  };
+  
+  export default prettierConfig;
+  
